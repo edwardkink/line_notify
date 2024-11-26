1eac1f4bcce7413602599cdf454b859619c7d507
kink.chen <kink.chen@hgiga.com>
2024-11-26 18:36:27 +0800

    [new]初始化

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+session_start();
+date_default_timezone_set("Asia/Taipei");
+
+include_once("lineNotify.class.php");
+
+define("CLIENT_ID","");
+define("CLIENT_SECRET","");
+define("CALLBACK_URL","http://localhost/LINE_Notify_PHP/callback.php");
+
+if(!(CLIENT_ID && CLIENT_SECRET && CALLBACK_URL)){
+	exit('請至config.php設定相關參數<br><a href="https://github.com/MTsung/LINE_Notify_PHP" target="_blank">github</a>');
+}
+$lineNotify = new lineNotify(CLIENT_ID,CLIENT_SECRET,CALLBACK_URL);
